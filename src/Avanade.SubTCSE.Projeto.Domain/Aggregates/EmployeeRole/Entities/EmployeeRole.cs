4b60e22fbe8ff46007e4b7da1dac140f8b67d466
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-06-15 18:36:12 -0300

    Criadas pastas dentro do projeto e class dentro da pasta Entities

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public class EmployeeRole
+    {
+        public string Id { get; private set; }
+        public string Rolename { get; private set; }
+    }
+}
