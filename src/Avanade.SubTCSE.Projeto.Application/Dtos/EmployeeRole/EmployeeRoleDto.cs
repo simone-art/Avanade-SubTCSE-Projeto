516a40aac10482e9ff061d107511e35933d8305b
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-06-21 15:45:40 -0300

    Criada classe DTO com seus atributos

---
+++ src/Avanade.SubTCSE.Projeto.Application/Dtos/EmployeeRole/EmployeeRoleDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Application.Dtos.EmployeeRole
+{
+    class EmployeeRoleDto
+    {
+    }
+}
