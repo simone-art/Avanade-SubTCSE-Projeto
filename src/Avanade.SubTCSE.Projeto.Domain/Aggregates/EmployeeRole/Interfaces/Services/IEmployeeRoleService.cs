848ed998c012ce84dad671a4f75c69e9f696344e
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-06-21 15:29:02 -0300

    Criada interface na pasta Repository

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services
 {
-    interface IEmployeeRoleService
+    public interface IEmployeeRoleService
     {
+        void AddEmployeeRole(Entities.EmployeeRole employeeRole);
     }
 }
--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
 {
-    class EmployeeRoleService
+    public class EmployeeRoleService : IEmployeeRoleService
     {
+        public void AddEmployeeRole(Entities.EmployeeRole employeeRole)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
