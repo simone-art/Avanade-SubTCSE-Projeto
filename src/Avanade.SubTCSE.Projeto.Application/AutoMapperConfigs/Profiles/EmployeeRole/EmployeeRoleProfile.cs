a47a5f87c53abb52d990f9c208238f24a589c9fa
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-06-21 18:18:44 -0300

    Criada class usando AutoMapper e fazendo mapeamento origem-destino

---
+++ src/Avanade.SubTCSE.Projeto.Application/AutoMapperConfigs/Profiles/EmployeeRole/EmployeeRoleProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Application.AutoMapperConfigs.Profiles.EmployeeRole
+{
+    public class EmployeeRoleProfile : Profile
+    {
+
+        public EmployeeRoleProfile()
+        {
+            //Fazendo mapeamento origem-destino
+            //Dtos.EmployeeRole.EmployeeRoleDto {origem}
+            //Domain.Aggregates.EmployeeRole.Entities.EmployeeRol {destino}
+            CreateMap<Dtos.EmployeeRole.EmployeeRoleDto, Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
+            .ForCtorParam("rolename", opt => opt.MapFrom(src => src.Cargo));
+        }
+    }
+}
