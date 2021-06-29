f87b068bdfbbc54ac01b01781fa15304993d1322
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-06-29 20:29:23 -0300

    Feitas correções de digitações

--- src/Avanade.SubTCSE.Projeto.Application/Interfaces/EmployeeRole/IEmployeeRoleAppService.cs
+++ src/Avanade.SubTCSE.Projeto.Application/Interfaces/EmployeeRole/IEmployeeRoleAppService.cs
@@ -1,8 +1,4 @@
 ﻿using Avanade.SubTCSE.Projeto.Application.Dtos.EmployeeRole;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Projeto.Application.Interfaces.EmployeeRole
@@ -10,6 +6,6 @@ namespace Avanade.SubTCSE.Projeto.Application.Interfaces.EmployeeRole
     public interface IEmployeeRoleAppService
     {
         //Task: tarefas que retornan instancias de objetos
-        Task<EmployeeRoleDto> AddeEmployeeRoleDto(EmployeeRoleDto employeeRoleDto);
+        Task<EmployeeRoleDto> AddEmployeeRoleDto(EmployeeRoleDto employeeRoleDto);
     }
 }
--- src/Avanade.SubTCSE.Projeto.Application/Services/EmployeeRole/EmployeeRoleAppService.cs
+++ src/Avanade.SubTCSE.Projeto.Application/Services/EmployeeRole/EmployeeRoleAppService.cs
@@ -1,9 +1,5 @@
 ﻿using Avanade.SubTCSE.Projeto.Application.Dtos.EmployeeRole;
 using Avanade.SubTCSE.Projeto.Application.Interfaces.EmployeeRole;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Projeto.Application.Services.EmployeeRole
@@ -11,9 +7,9 @@ namespace Avanade.SubTCSE.Projeto.Application.Services.EmployeeRole
     public class EmployeeRoleAppService : IEmployeeRoleAppService
 
     {
-        public Task<EmployeeRoleDto> AddeEmployeeRoleDto(EmployeeRoleDto employeeRoleDto)
+        public Task<EmployeeRoleDto> AddEmployeeRoleDto(EmployeeRoleDto employeeRoleDto)
         {
-            throw new NotImplementedException();
+            throw new System.NotImplementedException();
         }
     }
 }
