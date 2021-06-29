763d8be2c4ec3046df75433faefebcf094ecfdca
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-06-29 17:34:33 -0300

    Criada class EmployeeValidators na pasta Employee

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Validators/EmployeeValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Validators
+{
+    public class EmployeeValidator : AbstractValidator<Entities.Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleSet("new", () =>
+            {
+                RuleFor(e => e.FirstName)
+                 .NotEmpty()
+                 .WithMessage("{PropertyName} can not be empty");
+
+                RuleFor(e => e.SecondName)
+                .NotEmpty()
+                .WithMessage("{PropertyName} can not be empty");
+
+                //RuleFor(e => e.EmployeeRole)
+                //.SetValidator(new EmployeeRoleValidator());
+
+            });
+        }
+    }
+}
---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Validators/EmployeeRoleValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Validators
+{
+    public class EmployeeRoleValidator : AbstractValidator<Entities.Employee>
+    {
+        public EmployeeRoleValidator()
+        {
+            
+        }
+    }
+}
