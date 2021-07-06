274895c1670953526996a8f8eb6a8e1418a793dd
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-06 19:35:29 -0300

    Refactorado código

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Interfaces/Repositories/IEmployeeRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Interfaces/Repositories/IEmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Interfaces.Repositories
 {
-   public interface IEmployeeRepository
+   public interface IEmployeeRepository: IBaseRepository<Domain.Aggregates.Employee.Entities.Employee>
     {
-        Task<Employee.Entities.Employee> AddEmployee(Employee.Entities.Employee employee);
+        //Task<Employee.Entities.Employee> AddEmployee(Employee.Entities.Employee employee);
 
     }
 }
--- src/Avanade.SubTCSE.Projeto.Domain/Base/Repository/IBaseRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Base/Repository/IBaseRepository.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Projeto.Domain.Base.Repository
 {
-    interface IBaseRepository
+    public interface IBaseRepository<TEntity>
     {
+        Task<TEntity> Add(TEntity entity);
     }
 }
