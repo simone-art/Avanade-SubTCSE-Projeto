695cfa7a613135772316fb1886c51bf9373dfc67
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-06 20:17:54 -0300

    Feitas alterações

--- src/Avanade.SubTCSE.Projeto.Domain/Base/Repository/IBaseRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Base/Repository/IBaseRepository.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates;
+using System.Threading.Tasks;
+
 
 namespace Avanade.SubTCSE.Projeto.Domain.Base.Repository
 {
     public interface IBaseRepository<TEntity, Tid>
+        where TEntity : BaseEntity<Tid>
     {
         Task<TEntity> Add(TEntity entity);
 
--- src/Avanade.SubTCSE.Projeto.Infra.Data/Repositories/Base/MongoDB/MongoDBContext.cs
+++ src/Avanade.SubTCSE.Projeto.Infra.Data/Repositories/Base/MongoDB/MongoDBContext.cs
@@ -1,7 +1,13 @@
-﻿
+﻿using Avanade.SubTCSE.Projeto.Domain.Base.Repository.MongoDB;
+using MongoDB.Driver;
+
 namespace Avanade.SubTCSE.Projeto.Infra.Data.Repositories.Base.MongoDB
 {
-    public class MongoDBContext : IMongoDbContext
+    public class MongoDBContext : IMongoDBContext
     {
+        public IMongoCollection<TEntity> GetCollection<TEntity>(string collection)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
