3518828ee383c38ca1f02143eba3f7a84d9fa687
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-08 19:06:17 -0300

    Criadas pastas repository

---
+++ src/Avanade.SubTCSE.Projeto.Infra.Data/Repositories/EmployeeRole/EmployeeRoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Repository;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Infra.Data.Repositories.EmployeeRole
+{
+    public class EmployeeRoleRepository : 
+
+    {
+       
+    }
+}
